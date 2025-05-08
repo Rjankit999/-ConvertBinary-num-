5c10bb6d52df5c7f703c72fcfd570f162af4edcd
Rjankit999 <rjankitmaurya@gmail.com>
2025-05-08 10:59:23 +0530

    Add files via upload

---
+++ Project_5.py
@@ -0,0 +1,31 @@
+# # 
+# # Solve the quadratic equation ax**2 + bx + c = 0
+
+# # import complex math module
+# # import math
+
+# # a = 1
+# # b = 5
+# # c = 6
+
+# # # calculate the discriminant
+# # d = (b**2) - (4*a*c)
+# # # print(d)
+
+# # # find two solutions
+# # sol1 = (-b-math.sqrt(d))/(2*a)
+# # sol2 = (-b+math.sqrt(d))/(2*a)
+# # print(sol1)
+
+# # print('The solution are {0} and {1}'.format(sol1,sol2))
+# n=int(input("Enter NO.: "))
+
+# for i in range(1,11):
+#     print(n,"x",i,"=",n*i)
+
+i=165454654646879846
+while(1!=0):
+    print(i)
+    i=i+10000000000000000000000000000000000000000000000000000000000000 
+
+    
